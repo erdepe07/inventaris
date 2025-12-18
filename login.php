bc79f462f81f33bb231a461948af426fc77ad1d1
ryan dony p <86779526+erdepe07@users.noreply.github.com>
2025-12-18 14:04:05 +0700

    index

---
+++ login.php
@@ -0,0 +1,24 @@
+<?php
+    if(isset($_POST['login'])) {
+        
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php include "layout/header.php" ?>
+    <h3>MASUK AKUN</h3>
+    <form action="login.php" method="POST">
+        <input type="text" placeholder="username" name="username"/>
+        <input type="password" placeholder="password" name="password"/>
+        <button type="submit" name="login">masuk sekarang</button>
+    </form>
+    <?php include "layout/footer.php" ?>
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php include "layout/header.php" ?>
+    <h3>DAFTAR AKUN</h3>
+    <form>
+        <input type="text" placeholder="username" name="username"/>
+        <input type="password" placeholder="password" name="password"/>
+        <button type="submit">daftar sekarang</button>
+    </form>
+    <?php include "layout/footer.php" ?>
+</body>
+</html>
